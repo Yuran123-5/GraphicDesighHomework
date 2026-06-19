18876380fb7c9e105e867fa4a4bb258c6d0782eb
雨然 <1521661983@qq.com>
2026-06-19 19:08:34 +0800

    空间哈希优化时间复杂度
    利用了空间哈希去优化时间复杂度

--- Project/Assets/Script/particle.cs
+++ Project/Assets/Script/particle.cs
@@ -20,7 +20,7 @@ public class particle : MonoBehaviour
     private float boxRight;
 
     [Header("粒子设置")]
-    [Range(0,1000)]
+    [Range(0, 1000)]
     public int numParticles = 100;
     [Range(0, 11)]
     public float gravity;
@@ -32,7 +32,7 @@ public class particle : MonoBehaviour
     [Range(0, 1f)]
     public float spacing = 0f;
     [Range(0, 3f)]
-    public float smootherRadius=2f;
+    public float smootherRadius = 2f;
 
     public ParticleSystem.Particle[] particlesArray;
     public float[] particles_density;
@@ -40,13 +40,16 @@ public class particle : MonoBehaviour
     [Header("压强设置")]
     public float targetdensity;
     public float pressureMulitiper;
+
+    [Header("哈希计算")]
+    public Dictionary<int, List<int>> particles_dic;
     [Obsolete]
     void Start()
     {
         particlesSystem = GetComponent<ParticleSystem>();
         ParticleSystem.MainModule mainModule = particlesSystem.main;
         mainModule.maxParticles = 1000;
-        mainModule.startLifetime= Mathf.Infinity;
+        mainModule.startLifetime = Mathf.Infinity;
         mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
 
         boxBottom = initialPos.y - boxHeight / 2;
@@ -59,6 +62,7 @@ public class particle : MonoBehaviour
         particles_vel = new Vector2[numParticles];
         particles_density = new float[numParticles];
         initParticles();
+        initParticlesHash();
         updateDensity();
     }
     void initParticles()
@@ -86,12 +90,12 @@ public class particle : MonoBehaviour
             particles_pos[i] = pos;
             particles_vel[i] = vel;
 
-            ParticleSystem.Particle particleData=new ParticleSystem.Particle();
+            ParticleSystem.Particle particleData = new ParticleSystem.Particle();
             particleData.position = pos;
             particleData.startSize = radius;
             particleData.startColor = particalColor;
 
-            particlesArray[i]=particleData;
+            particlesArray[i] = particleData;
         }
         particlesSystem.SetParticles(particlesArray, particlesArray.Length);
         //particlesSystem.Play();
@@ -99,32 +103,33 @@ public class particle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        updateHashGrid();
         updateDensity();
         updateParticles();
     }
     public void updateParticles()
     {
-        for(int i = 0; i <numParticles; i++)
+        for (int i = 0; i < numParticles; i++)
         {
-            Vector2 pos=particles_pos[i];
-            Vector2 vel=particles_vel[i];
-            float density=particles_density[i];
-            updateVelocity(ref pos, ref vel,i);
+            Vector2 pos = particles_pos[i];
+            Vector2 vel = particles_vel[i];
+            float density = particles_density[i];
+            updateVelocity(ref pos, ref vel, i);
             particles_pos[i] = pos;
             particles_vel[i] = vel;
             ParticleSystem.Particle particle = particlesArray[i];
             particle.position = pos;
-            particle.startColor=particalColor;
+            particle.startColor = particalColor;
             particle.startSize = radius;
-            particlesArray[i]=particle;
+            particlesArray[i] = particle;
         }
         particlesSystem.SetParticles(particlesArray, particlesArray.Length);
     }
-    public void updateVelocity(ref Vector2 pos,ref Vector2 vel,int index)
+    public void updateVelocity(ref Vector2 pos, ref Vector2 vel, int index)
     {
-        vel+= Vector2.down * gravity * Time.deltaTime;
-        float density=particles_density[index];
-        Vector2 pressureForce=calculatePressureForce(index);
+        vel += Vector2.down * gravity * Time.deltaTime;
+        float density = particles_density[index];
+        Vector2 pressureForce = calculatePressureForce(index);
         Vector2 pressureForceAcceleration = pressureForce / density;
         vel += pressureForceAcceleration * Time.deltaTime;
         pos += vel * Time.deltaTime;
@@ -161,53 +166,88 @@ public class particle : MonoBehaviour
         Gizmos.DrawWireCube(center, size);
     }
 
-    public float SmoothKerneel(float smoothRadius,float distance)
+    public float SmoothKerneel(float smoothRadius, float distance)
     {
         if (distance >= smoothRadius)
             return 0;
         float volume = MathF.PI * MathF.Pow(smoothRadius, 4) / 6;
-        return (smoothRadius-distance)* (smoothRadius - distance) /volume;
+        return (smoothRadius - distance) * (smoothRadius - distance) / volume;
     }
-    public float smoothKerneelDerivative(float smoothRadius,float distance)
+    public float smoothKerneelDerivative(float smoothRadius, float distance)
     {
-        if(distance>=smoothRadius)
+        if (distance >= smoothRadius)
             return 0;
-        float f=  smoothRadius -distance;
+        float f = smoothRadius - distance;
         float scale = -12 / (Mathf.PI * Mathf.Pow(smoothRadius, 4));
-        return scale*  f;
+        return scale * f;
     }
-    public float calculateDensity(Vector2 samplePoint) 
+    public float calculateDensity(Vector2 samplePoint)
     {
         float density = 0;
         const int mass = 1;
-        for (int i = 0; i < particles_pos.Length; i++) 
+        for (int i = 0; i < particles_pos.Length; i++)
         {
-            float distance = (particles_pos[i]-samplePoint).magnitude;
-            float influence=SmoothKerneel(smootherRadius,distance);
-            density += influence*mass;
+            float distance = (particles_pos[i] - samplePoint).magnitude;
+            float influence = SmoothKerneel(smootherRadius, distance);
+            density += influence * mass;
+        }
+        return density;
+    }
+    public float calculateDensityByHash(Vector2 samplePoint)
+    {
+        float density = 0;
+        const int mass = 1;
+        HashSet<int> surroundHash = new HashSet<int>();
+        int[] dx = { 0, 0, -1, 1, 1, 1, -1, -1, 0 };
+        int[] dy = { 1, -1, 0, 0, 1, -1, 1, -1, 0 };
+        Vector2 CellPos=calculateCellPos(samplePoint);
+        for (int i = 0; i < 9; i++)
+        {
+            Vector2 dir = new Vector2(dx[i], dy[i]);
+            Vector2 surroundGrid = CellPos + dir;
+            int hashkey = calculateCellHashKey(surroundGrid);
+            surroundHash.Add(hashkey);
+        }
+        List<int> surroundParticlesByHash = new List<int>();
+        foreach (int hashkey in surroundHash)
+        {
+            if (!particles_dic.ContainsKey(hashkey))
+                continue;
+            List<int> particlesByHash = particles_dic[hashkey];
+            for (int i = 0; i < particlesByHash.Count; i++)
+            {
+                surroundParticlesByHash.Add(particlesByHash[i]);
+            }
+        }
+        for (int i = 0; i < surroundParticlesByHash.Count; i++)
+        {
+            float distance = (particles_pos[surroundParticlesByHash[i]] - samplePoint).magnitude;
+            float influence = SmoothKerneel(smootherRadius, distance);
+            density += influence * mass;
         }
         return density;
     }
     public void updateDensity()
     {
-        for (int i = 0;i < particles_density.Length;i++)
+        for (int i = 0; i < particles_density.Length; i++)
         {
-            float density=particles_density[i];
-            density=calculateDensity(particles_pos[i]);
-            particles_density[i]=density;
+            float density = particles_density[i];
+            density = calculateDensityByHash(particles_pos[i]);
+            particles_density[i] = density;
         }
     }
     public Vector2 calculatePressureForce(int index)
     {
         Vector2 pressureForce = Vector2.zero;
         const int mass = 1;
-        Vector2 samplePoint=particles_pos[index];
-        for (int i = 0; i < numParticles; i++) {
+        Vector2 samplePoint = particles_pos[index];
+        for (int i = 0; i < numParticles; i++)
+        {
             if (index == i)
                 continue;
-            float dst=(particles_pos[i]-samplePoint).magnitude;
+            float dst = (particles_pos[i] - samplePoint).magnitude;
 
-            Vector2 dir= dst==0?GetRandomDir():(particles_pos[i] - samplePoint)/dst;
+            Vector2 dir = dst == 0 ? GetRandomDir() : (particles_pos[i] - samplePoint) / dst;
             float slope = smoothKerneelDerivative(smootherRadius, dst);
             float density = particles_density[i];
             float sharedPressure = calculateSharedPress(density, particles_density[index]);
@@ -217,7 +257,7 @@ public class particle : MonoBehaviour
     }
     public float converDensityToPressure(float density)
     {
-        float densityError=density-targetdensity;
+        float densityError = density - targetdensity;
         float pressure = densityError * pressureMulitiper;
         return pressure;
     }
@@ -225,18 +265,47 @@ public class particle : MonoBehaviour
     {
         float x = 0;
         float y = 0;
-        while(x==0&&y==0)
+        while (x == 0 && y == 0)
         {
             x = UnityEngine.Random.Range(-1, 1);
             y = UnityEngine.Random.Range(-1, 1);
         }
-        float dst=new Vector2(x,y).magnitude;
-        return new Vector2(x,y)/dst;
+        float dst = new Vector2(x, y).magnitude;
+        return new Vector2(x, y) / dst;
     }
     public float calculateSharedPress(float densitya, float densityb)
     {
         float pressurea = converDensityToPressure(densitya);
         float pressureb = converDensityToPressure(densityb);
         return (pressurea + pressureb) / 2;
+    }
+    public Vector2 calculateCellPos(Vector2 pos)
+    {
+        int x = (int)(pos.x / radius);
+        int y = (int)(pos.y / radius);
+        return new Vector2(x, y);
+    }
+    public int calculateCellHashKey(Vector2 CellPos)
+    {
+        return (int)((uint)(CellPos.x * 42688201 + CellPos.y * 79193993) % 10);
+    }
+    public void initParticlesHash()
+    {
+        particles_dic = new Dictionary<int, List<int>>();
+        updateHashGrid();   
+    }
+    public void updateHashGrid()
+    {
+        particles_dic.Clear();
+        for (int i = 0; i < numParticles; i++)
+        {
+            Vector2 pos = particles_pos[i];
+            int hashKey = calculateCellHashKey(calculateCellPos(pos));
+            if (!particles_dic.ContainsKey(hashKey))
+            {
+                particles_dic[hashKey] = new List<int>();
+            }
+            particles_dic[hashKey].Add(i);
+        }
     }
 }
